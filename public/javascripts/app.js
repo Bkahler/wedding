e9af4a864ded8f6494905809ef55d69d4850db93
Bryan Kahler <bkahler3@gmail.com>
2016-03-05 19:41:46 +0000

    adding in map

---
+++ public/javascripts/app.js
@@ -0,0 +1,5 @@
+$(function() {
+    L.mapbox.accessToken = 'pk.eyJ1IjoiYmthaGxlciIsImEiOiJjaWxmaHNwZHkxcXVydHZrcHp5cmE4bHM5In0.dKkkzvffVWeAhlsq8cnuww';
+    var map = L.mapbox.map('map', 'bkahler.pb42p70m');
+})
+
