87b7484ac411654a813d760d173f6e64ab70e09f
Bryan Kahler <bkahler3@gmail.com>
2016-03-05 20:04:35 +0000

    adding more style

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,13 @@
 $(function() {
     L.mapbox.accessToken = 'pk.eyJ1IjoiYmthaGxlciIsImEiOiJjaWxmaHNwZHkxcXVydHZrcHp5cmE4bHM5In0.dKkkzvffVWeAhlsq8cnuww';
     var map = L.mapbox.map('map', 'bkahler.pb42p70m');
+    
+    
+    $("#rsvp-toggle").on("click", function(){
+    	$("#rsvp-form").fadeToggle(2000, function(){
+    		console.log("Toggled!")
+    	});
+    })
+    
 })
 
