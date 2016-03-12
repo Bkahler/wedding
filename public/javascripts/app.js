c1b79711028188a5fa210dc9546b97415401890e
Bryan Kahler <bkahler3@gmail.com>
2016-03-12 23:03:34 +0000

    figured out container fade

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,18 +1,24 @@
+$(document).ready(wait()); 
 $(function() {
     L.mapbox.accessToken = 'pk.eyJ1IjoiYmthaGxlciIsImEiOiJjaWxmaHNwZHkxcXVydHZrcHp5cmE4bHM5In0.dKkkzvffVWeAhlsq8cnuww';
     var map = L.mapbox.map('map', 'bkahler.pb42p70m');
     
-    
     $("#rsvp-toggle").on("click", function(){
     	$("#rsvp-div").fadeToggle(2000, function(){
-    		console.log("Toggled!")
+    		console.log("Toggled!");
     	});
     	$("#rsvp-toggle").fadeToggle(2000, function(){
-    		console.log("Toggled!")
+    		console.log("Toggled!");
     	});
     });
-    
-    // $("#success").delay(1000).fadeOut();
-    
-})
+        
+});
 
+
+function wait(){
+  setTimeout(fade_flash, 2000);
+}
+
+function fade_flash() {
+  $('#flash').fadeOut();
+}
