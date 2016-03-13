fa96f53cf724e54d89e743533cd9bf20678c319d
Bryan Kahler <bkahler3@gmail.com>
2016-03-13 01:22:12 +0000

    using chart.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,40 +1,70 @@
-$(document).ready(wait()); 
-$(function() {
+$(document).ready(
+    wait()
+  ); 
+
+function addMap(){
     L.mapbox.accessToken = 'pk.eyJ1IjoiYmthaGxlciIsImEiOiJjaWxmaHNwZHkxcXVydHZrcHp5cmE4bHM5In0.dKkkzvffVWeAhlsq8cnuww';
     var map = L.mapbox.map('map', 'bkahler.pb42p70m');
-    
+};
+
+function addClickHandlers(){
     $("#rsvp-toggle").on("click", function(){
+      
     	$("#rsvp-div").fadeToggle(2000, function(){
     		console.log("Toggled!");
     	});
+    	
     	$("#rsvp-toggle").fadeToggle(2000, function(){
     		console.log("Toggled!");
     	});
+    	
     });
-    
-    
-    var data = {
-  // A labels array that can contain any sort of values
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  // Our series array that contains series objects or in this case series data arrays
-  series: [
-    [5, 2, 4, 2, 0]
-  ]
-};
+}
 
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
-new Chartist.Line('.ct-chart', data);
-        
-});
-
+function chartPrep() {
+    $("#showChart").click(function(){
+    	
+    	$("#chart").fadeToggle(2000, function(){
+    		console.log("Toggled!");
+    	});
+    	
+      addChart();
+    });
+}
 
 function wait(){
   setTimeout(fade_flash, 2000);
+  setTimeout(chartPrep, 1000);
+  setTimeout(addClickHandlers, 500);
+  setTimeout(addMap,500);
 }
 
 function fade_flash() {
   $('#flash').fadeOut();
 }
+
+function addChart(){
+  var pieData = [
+     {
+        value: 25,
+        label: 'NO',
+        color: '#811BD6'
+     },
+     {
+        value: 10,
+        label: 'YES',
+        color: '#9CBABA'
+     },     
+     {
+        value: 60,
+        label: '??',
+        color: '#1abc9c'
+     }
+  ];
+  
+  var context = document.getElementById('chart').getContext('2d');
+  var skillsChart = new Chart(context).Pie(pieData);
+  
+}
+
 
